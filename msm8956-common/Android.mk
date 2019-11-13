1c1060f903f8f7fc55a539934b9c3abc63e7b9c2
kubersharma001 <v@users.noreply.github.com>
2019-11-13 18:34:10 +0000

    msm8956-common: Update blobs from mata Q
    * mata-user 10 QP1A.190711.122 420 release-keys

--- msm8956-common/Android.mk
+++ msm8956-common/Android.mk
@@ -173,7 +173,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ims
-LOCAL_MODULE_OWNER := xiaomi
+LOCAL_MODULE_OWNER := oneplus
 LOCAL_SRC_FILES := proprietary/priv-app/ims/ims.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
