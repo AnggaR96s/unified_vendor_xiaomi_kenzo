c4d59ab4e963d8d4abd3f5040cb8f9f9a5068e3d
Magicxavi <rolfpeterkesche@gmail.com>
2020-11-09 10:32:52 +0200

    Revert "kenzo: add missing lib needed for new keystore"
    This reverts commit 71a84bdb8408c60fa22d8608ef6e513d35f72afd.
    
    Signed-off-by: Angga <angga@linuxmail.org>

--- kenzo-vendor.mk
+++ kenzo-vendor.mk
@@ -759,7 +759,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/kenzo/proprietary/vendor/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so \
     vendor/xiaomi/kenzo/proprietary/vendor/lib64/hw/gatekeeper.msm8952.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gatekeeper.msm8952.so \
     vendor/xiaomi/kenzo/proprietary/vendor/lib64/hw/keystore.msm8952.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/keystore.msm8952.so \
-    vendor/xiaomi/kenzo/proprietary/vendor/lib64/libcrypto_keystore.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto_keystore.so \
     vendor/xiaomi/kenzo/proprietary/vendor/lib64/hw/vulkan.msm8952.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vulkan.msm8952.so \
     vendor/xiaomi/kenzo/proprietary/vendor/lib64/lib-dplmedia.so:$(TARGET_COPY_OUT_VENDOR)/lib64/lib-dplmedia.so \
     vendor/xiaomi/kenzo/proprietary/vendor/lib64/lib-imsSDP.so:$(TARGET_COPY_OUT_VENDOR)/lib64/lib-imsSDP.so \
